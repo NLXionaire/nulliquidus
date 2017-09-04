445fc9ffcb9e687a9dbf3162ae0b0c89612f014c
Lucas Buzzo <lcsbzo@gmail.com>
2017-09-04 12:28:53 -0300

    Show masternodes count.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var express = require('express')
   , locale = require('../lib/locale')
   , db = require('../lib/database')
   , lib = require('../lib/explorer')
+  , masternodes = require('../lib/masternodes')
   , qr = require('qr-image');
 
 function route_get_block(res, blockhash) {
@@ -279,6 +280,13 @@ router.get('/qr/:string', function(req, res) {
     res.type('png');
     address.pipe(res);
   }
+});
+
+router.get('/ext/masternodes', function(req, res) {
+  console.log(masternodes.getMasternodesCount());
+  //console.log(contagem);
+  console.log(masternodes.masternodesCount);
+  res.send(masternodes.getMasternodesCount());
 });
 
 router.get('/ext/summary', function(req, res) {
