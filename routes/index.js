b4e48d553c22af69ca15bf33645c4ada8ea29f1a
Alan Rudolf <@suprnurd>
2017-09-09 06:49:48 -0400

    add master node count

--- routes/index.js
+++ routes/index.js
@@ -304,20 +304,23 @@ router.get('/ext/summary', function(req, res) {
     }
     lib.get_hashrate(function(hashrate) {
       lib.get_connectioncount(function(connections){
-        lib.get_blockcount(function(blockcount) {
-          db.get_stats(settings.coin, function (stats) {
-            if (hashrate == 'There was an error. Check your console.') {
-              hashrate = 0;
-            }
-            res.send({ data: [{
-              difficulty: difficulty,
-              difficultyHybrid: difficultyHybrid,
-              supply: stats.supply,
-              hashrate: hashrate,
-              lastPrice: stats.last_price,
-              connections: connections,
-              blockcount: blockcount
-            }]});
+        lib.get_masternodecount(function(masternodes){
+          lib.get_blockcount(function(blockcount) {
+            db.get_stats(settings.coin, function (stats) {
+              if (hashrate == 'There was an error. Check your console.') {
+                hashrate = 0;
+              }
+              res.send({ data: [{
+                difficulty: difficulty,
+                difficultyHybrid: difficultyHybrid,
+                supply: stats.supply,
+                hashrate: hashrate,
+                lastPrice: stats.last_price,
+                connections: connections,
+                masternodesCount: masternodes,
+                blockcount: blockcount
+              }]});
+            });
           });
         });
       });
